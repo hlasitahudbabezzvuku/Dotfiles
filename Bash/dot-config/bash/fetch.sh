cc0c4c1e92bfab9f5eabbd75cdd826a87a3d8395
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-10-02 11:17:47 +0200

    Add simple fetch script

---
+++ Bash/dot-config/bash/fetch.sh
@@ -0,0 +1,12 @@
+printf "\n    _/        _/        _/               "
+printf "\n   _/_/_/    _/_/_/    _/_/_/    _/_/_/_/"
+printf "\n  _/    _/  _/    _/  _/    _/      _/   "
+printf "\n _/    _/  _/    _/  _/    _/    _/      "
+printf "\n_/    _/  _/    _/  _/_/_/    _/_/_/_/   \n"
+
+printf "\n┌───────── HlasitaHudbaBezZvuku ─────────┐"
+printf "\n  user:       %s" "$( whoami )"
+printf "\n  hostname:   %s" "$( hostname )"
+printf "\n  uptime:     %s" "$( uptime -p )"
+printf "\n└────────────────────────────────────────┘\n"
+
