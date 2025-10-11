aecf4962db628c01af1e61cfcd3410dd24fe7066
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-10-11 15:51:58 +0200

    Add colors to fetch.sh

--- Bash/dot-config/bash/fetch.sh
+++ Bash/dot-config/bash/fetch.sh
@@ -1,12 +1,12 @@
-printf "\n    _/        _/        _/               "
-printf "\n   _/_/_/    _/_/_/    _/_/_/    _/_/_/_/"
-printf "\n  _/    _/  _/    _/  _/    _/      _/   "
-printf "\n _/    _/  _/    _/  _/    _/    _/      "
-printf "\n_/    _/  _/    _/  _/_/_/    _/_/_/_/   \n"
+printf '\n    _/        _/        _/               '
+printf '\n   _/_/_/    _/_/_/    _/_/_/    _/_/_/_/'
+printf '\n  _/    _/  _/    _/  _/    _/      _/   '
+printf '\n _/    _/  _/    _/  _/    _/    _/      '
+printf '\n_/    _/  _/    _/  _/_/_/    _/_/_/_/   \n'
 
-printf "\n┌─────────── HlasitaHudbaBezZvuku ───────────┐"
-printf "\n  user:       %s" "$( whoami )"
-printf "\n  hostname:   %s" "$( hostname )"
-printf "\n  uptime:     %s" "$( uptime -p )"
-printf "\n└────────────────────────────────────────────┘\n"
+printf '\n%s┌─────────── %sHlasitaHudbaBezZvuku%s ───────────┐%s' "$( tput setaf 8 )" "$( tput setaf 4 )" "$( tput setaf 8 )" "$( tput sgr0 )"
+printf '\n  user:       %s' "$( whoami )"
+printf '\n  hostname:   %s' "$( hostname )"
+printf '\n  uptime:     %s' "$( uptime -p )"
+printf '\n%s└────────────────────────────────────────────┘%s\n' "$( tput setaf 8 )" "$( tput sgr0 )"
 
