3cff92af9c6687d5b46e6716ff8a549745214558
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-10-20 14:27:16 +0200

    Remake the donwload.sh script

--- Bash/dot-local/bin/download.sh
+++ Bash/dot-local/bin/download.sh
@@ -2,11 +2,28 @@
 
 declare -r DOWNLOADER="yt-dlp"
 
-declare -r ARGS_GENERAL='--force-ipv4 --no-flat-playlist --yes-playlist --continue --part --progress'
-declare -r ARGS_FORMAT='~/Media/Music/%(playlist|artist)s/%(playlist_index&{}. |)s%(title)s.%(ext)s'
-declare -r ARGS_AUDIO='--extract-audio --audio-format mp3 --audio-quality 5 --embed-thumbnail --embed-metadata'
-
 for url in "${@}"; do
-    ${DOWNLOADER} ${ARGS_GENERAL} --output "${ARGS_FORMAT}" ${ARGS_AUDIO} "${url}"
+    while ! ${DOWNLOADER} \
+        --abort-on-error \
+        --force-ipv4 \
+        --no-flat-playlist \
+        --yes-playlist \
+        --continue \
+        --part \
+        --progress \
+        --extract-audio \
+        --audio-format mp3 \
+        --audio-quality 5 \
+        --embed-thumbnail \
+        --embed-metadata \
+        --convert-thumbnails jpg \
+        --ppa 'EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop="'"'"'if(gt(ih,iw),iw,ih)'"':'"'if(gt(iw,ih),ih,iw)'"'"'"' \
+        --output '~/Media/Music/%(playlist|artist)s/%(playlist_index&{}. |)s%(title)s.%(ext)s' \
+        "${url}"
+    do
+        sleep $(( 60 * 5 ))
+    done
+
+    sleep 30
 done
 
