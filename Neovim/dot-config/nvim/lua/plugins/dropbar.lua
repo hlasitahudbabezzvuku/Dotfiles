74c7b07766b88c656bcef1af85209562678a73b0
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-11-05 12:28:24 +0100

    Remove nvim dropbar

--- Neovim/dot-config/nvim/lua/plugins/dropbar.lua
+++
@@ -1,9 +0,0 @@
-vim.keymap.set('n', '<leader>j', function() require('dropbar.api').pick() end)
-vim.keymap.set('n', '<leader>[', function() require('dropbar.api').goto_context_start() end)
-vim.keymap.set('n', '<leader>]', function() require('dropbar.api').select_next_context() end)
-
-return {
-    'Bekaboo/dropbar.nvim',
-    event = 'BufRead',
-    opts = {}
-}
