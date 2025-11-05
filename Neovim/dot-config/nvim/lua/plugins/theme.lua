a9054fda5da82a35b3b8e54fbcb81f2147fe4e64
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-11-05 16:44:33 +0100

    Add custom colors for noice.nvim

--- Neovim/dot-config/nvim/lua/plugins/theme.lua
+++ Neovim/dot-config/nvim/lua/plugins/theme.lua
@@ -17,8 +17,19 @@ return {
             groups = {
                 all = {
                     TelescopeBorder = { fg = 'bg2' },
-                    TelescopePromptTitle = { bg = 'palette.red', fg =  'bg0'},
-                    TelescopePreviewTitle = { bg = 'palette.blue', fg =  'bg0'},
+                    TelescopePromptTitle = { bg = 'palette.red', fg = 'bg0'},
+                    TelescopePreviewTitle = { bg = 'palette.blue', fg = 'bg0'},
+                    NoiceCmdlinePopupBorderCmdline = { fg = 'bg2' },
+                    NoiceCmdlinePopupBorderHelp = { fg = 'bg2' },
+                    NoiceCmdlinePopupBorderLua = { fg = 'bg2' },
+                    NoiceCmdlinePopupBorderSearch = { fg = 'bg2' },
+                    NoiceCmdlinePopupTitleCmdline = { bg = 'palette.red', fg = 'bg0'},
+                    NoiceCmdlinePopupTitleHelp = { bg = 'palette.blue', fg = 'bg0'},
+                    NoiceCmdlinePopupTitleLua = { bg = 'palette.blue', fg = 'bg0'},
+                    NoiceCmdlinePopupTitleSearch = { bg = 'palette.orange', fg = 'bg0'},
+                    NoiceCmdlineIcon = { fg = 'palette.blue' },
+                    NoiceCmdlineIconCmdline = { fg = 'palette.red' },
+                    NoiceCmdlineIconSearch = { fg = 'palette.orange' },
                 },
             },
         })
