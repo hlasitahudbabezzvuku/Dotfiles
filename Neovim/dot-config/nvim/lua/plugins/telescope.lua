72a05a9ccef7993d067e364a8088114f584c242b
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-11-05 16:45:31 +0100

    Remove binding for telescopes cmd

--- Neovim/dot-config/nvim/lua/plugins/telescope.lua
+++ Neovim/dot-config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,4 @@
-vim.keymap.set('n', '<leader>h', function() require('telescope.builtin').builtin() end)
-
-vim.keymap.set('n', '<leader>;', function() require('telescope.builtin').commands() end)
+vim.keymap.set('n', '<leader>;', function() require('telescope.builtin').builtin() end)
 vim.keymap.set('n', '<leader>f', function() require('telescope.builtin').find_files() end)
 vim.keymap.set('n', '<leader>g', function() require('telescope.builtin').live_grep() end)
 vim.keymap.set('n', '<leader>b', function() require('telescope.builtin').buffers() end)
