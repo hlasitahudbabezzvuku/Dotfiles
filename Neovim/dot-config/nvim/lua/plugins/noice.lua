52d8cdac7beb7800e75c651a866ea3806347b6de
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-11-05 17:18:26 +0100

    Add noice.nvim configuration

--- Neovim/dot-config/nvim/lua/plugins/noice.lua
+++ Neovim/dot-config/nvim/lua/plugins/noice.lua
@@ -1,9 +1,39 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
+    'folke/noice.nvim',
+    lazy = false,
     dependencies = {
-        "MunifTanjim/nui.nvim",
-        -- "rcarriga/nvim-notify",
+        'MunifTanjim/nui.nvim',
+        -- 'rcarriga/nvim-notify',
+        'nvim-treesitter/nvim-treesitter',
     },
-    opts = {},
+    opts = {
+        lsp = {
+            override = {
+                ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                ['vim.lsp.util.stylize_markdown'] = true,
+                ['cmp.entry.get_documentation'] = true,
+            },
+        },
+        views = {
+            cmdline_popup = {
+                position = { row = 14, col = '50%' },
+                size = { width = 76, height = 'auto' },
+            },
+            popupmenu = {
+                position = { row = 17, col = '50%' },
+                size = { width = 76, height = 12 },
+                border = {
+                    style = 'rounded',
+                    padding = { 0, 1 },
+                },
+                win_options = {
+                    winhighlight = { Normal = 'Normal', FloatBorder = 'FloatBorder' },
+                },
+            },
+        },
+        presets = {
+            long_message_to_split = true,
+            lsp_doc_border = true,
+        },
+    },
 }
