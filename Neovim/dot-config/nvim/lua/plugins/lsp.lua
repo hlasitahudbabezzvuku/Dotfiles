c1f4112d7ecbd7f5dc2fd292f0b5fa9064476eb2
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-11-13 09:50:31 +0100

    Reformated configuration for lsp in Neovim

--- Neovim/dot-config/nvim/lua/plugins/lsp.lua
+++ Neovim/dot-config/nvim/lua/plugins/lsp.lua
@@ -1,24 +1,11 @@
 return {
     'neovim/nvim-lspconfig',
     event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = { 'williamboman/mason-lspconfig.nvim' },
-    config = function()
-        vim.api.nvim_create_autocmd('LspAttach', {
-            callback = function(event)
-                local opts = { buffer = event.buf }
-                vim.keymap.set({ 'n', 'i', 'x' }, '<a-cr>', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                vim.keymap.set({ 'n', 'i', 'x' }, '<f2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                vim.keymap.set({ 'n', 'i', 'x' }, '<f3>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-                vim.keymap.set({ 'n', 'i', 'x' }, '<f4>', '<cmd>lua vim.lsp.buf.format()<cr>', opts)
-                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-            end,
-        })
-
+    dependencies = {
+        "mason.nvim",
+        "mason-org/mason-lspconfig.nvim",
+    },
+    config = function ()
         vim.diagnostic.config({
             virtual_text = { prefix = ' ' },
             underline = true,
@@ -32,6 +19,20 @@ return {
                     [vim.diagnostic.severity.HINT] = '',
                 },
             },
+        })
+        vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(event)
+                local opts = { buffer = event.buf }
+                vim.keymap.set({ 'n', 'i', 'x' }, '<f2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                vim.keymap.set({ 'n', 'i', 'x' }, '<f3>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+                vim.keymap.set({ 'n', 'i', 'x' }, '<f4>', '<cmd>lua vim.lsp.buf.format()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+            end,
         })
     end
 }
