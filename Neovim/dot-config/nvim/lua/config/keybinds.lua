b295a0c9f37c8ac41b16c9deec9d3414d28b191d
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-11-21 23:57:16 +0100

    Add descriptions to some keybinds and remove unneded ones

--- Neovim/dot-config/nvim/lua/config/keybinds.lua
+++ Neovim/dot-config/nvim/lua/config/keybinds.lua
@@ -1,26 +1,19 @@
 -- Files
-vim.keymap.set('n', '<leader>e', '<cmd>Explore<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>e', '<cmd>Explore<cr>', { noremap = true, silent = true, desc = "Open netrw" })
 
-vim.keymap.set({ 'n', 'i', 'x', 'v', 't' }, '<c-s>', '<cmd>write<cr>', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'i', 'x', 'v', 't' }, '<c-q>', '<cmd>quitall<cr>', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'i', 'x', 'v', 't' }, '<c-s>', '<cmd>write<cr>', { noremap = true, silent = true, desc = 'Save the current buffer' })
+vim.keymap.set({ 'n', 'i', 'x', 'v', 't' }, '<c-q>', '<cmd>quitall<cr>', { noremap = true, silent = true, desc = 'Quit the current buffer'  })
 
 -- Directories
-vim.keymap.set('n', '<leader>cc', function() vim.cmd('cd ' .. vim.fn.expand('%:p:h')) end, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>cu', function() vim.cmd('cd ' .. vim.fn.expand('..')) end, { noremap = true, silent = true })
-
--- Buffers
-vim.keymap.set('n', '<leader>d', '<cmd>bdelete<cr>', { noremap = true, silent = true })
-
--- Tabs
-vim.keymap.set('n', '<leader>n', '<cmd>tabnext<cr>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>p', '<cmd>tabprevious<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>cc', function() vim.cmd('cd ' .. vim.fn.expand('%:p:h')) end, { noremap = true, silent = true, desc = 'Change root to this file'  })
+vim.keymap.set('n', '<leader>cu', function() vim.cmd('cd ' .. vim.fn.expand('..')) end, { noremap = true, silent = true, desc = 'Change root to one directory up'  })
 
 -- QoL
 vim.keymap.set('n', '<esc>', '<cmd>nohlsearch<cr>')
 
 -- Tools
-vim.keymap.set('n', '<leader>ll', '<cmd>Lazy<cr>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>lm', '<cmd>Mason<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ll', '<cmd>Lazy<cr>', { noremap = true, silent = true, desc = 'Open Lazy' })
+vim.keymap.set('n', '<leader>lm', '<cmd>Mason<cr>', { noremap = true, silent = true, desc = 'Open Mason' })
 
 -- Disable arrows
 vim.keymap.set({ 'n', 'i', 'x', 'v' }, '<up>', '<nop>', { noremap = true, silent = true })
