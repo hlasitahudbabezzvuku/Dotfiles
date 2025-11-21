aab3c36c3ae1e3b5d4ce20bbedcc112e3966b7f0
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-11-21 23:00:26 +0100

    Add configuration to notifications in Neovim

--- Neovim/dot-config/nvim/lua/plugins/notify.lua
+++ Neovim/dot-config/nvim/lua/plugins/notify.lua
@@ -3,5 +3,9 @@ return {
     lazy = false,
     opts = {
         background_colour = "#000000",
-    }
+        timeout = 2000,
+        render = "default",
+        stages = "static",
+        top_down = false,
+    },
 }
