2cde443c7f5c5fa0c3f0af18b5cf62d747a7db95
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-11-21 22:21:41 +0100

    Replace fidget.nvim with nvim-notify

--- Neovim/dot-config/nvim/lua/plugins/notify.lua
+++ Neovim/dot-config/nvim/lua/plugins/notify.lua
@@ -1,18 +1,7 @@
 return {
-    'j-hui/fidget.nvim',
+    'rcarriga/nvim-notify',
     lazy = false,
-    config = function()
-        require('fidget').setup({
-            notification = {
-                override_vim_notify = true,
-                window = {
-                    winblend = 0,
-                    border = 'none',
-                    x_padding = 2,
-                    y_padding = 1,
-                    align = 'bottom',
-                },
-            },
-        })
-    end
+    opts = {
+        background_colour = "#000000",
+    }
 }
