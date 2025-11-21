08ee704c594caa8c991a28d0f844cc3dd31e4765
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-11-21 21:18:22 +0100

    Add cmd specifiers to the overseer.nvim config

--- Neovim/dot-config/nvim/lua/plugins/overseer.lua
+++ Neovim/dot-config/nvim/lua/plugins/overseer.lua
@@ -1,4 +1,12 @@
 return {
     'stevearc/overseer.nvim',
+    cmd = {
+        'OverseerClose',
+        'OverseerOpen',
+        'OverseerRun',
+        'OverseerShell',
+        'OverseerTaskAction',
+        'OverseerToggle',
+    },
     opts = {},
 }
