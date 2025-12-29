4d98ada17a82c75db21f8e0274edca5f4442a10a
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-12-29 17:14:26 +0100

    Fix the plasma workspace script

--- Plasma/dot-local/bin/workspace.sh
+++ Plasma/dot-local/bin/workspace.sh
@@ -5,10 +5,12 @@ rows_count="$( qdbus-qt6 org.kde.KWin  /VirtualDesktopManager rows )"
 desktops_per_row="$(( desktops_count / rows_count ))"
 
 current_desktop="$(( $( qdbus-qt6 org.kde.KWin /KWin currentDesktop ) - 1 ))"
-current_row="$(( current_desktop / rows_count ))"
+current_row="$(( current_desktop / desktops_per_row ))"
+currnet_column="$(( current_desktop % desktops_per_row ))"
 
 if [[ "${1}" -le 0 ]] || [[ "${1}" -gt "${desktops_per_row}" ]]; then
-    printf "[error]: The suplied index is out of range\n"
+    printf "[\e[31merror\e[0m]: The suplied index is out of range\n"
+    printf "desktop-%i:\n\tDESKTOPS: %i\n\tROWS:     %i\n\tDPR:      %i\n\tCDESKTOP: %i\n\tCROW:     %i\n\tCCOL:     %i\n" "$current_desktop" "$desktops_count" "$rows_count" "$desktops_per_row" "$(( current_desktop + 1 ))" "$(( current_row + 1 ))" "$(( currnet_column + 1 ))"
     exit 1
 fi
 
