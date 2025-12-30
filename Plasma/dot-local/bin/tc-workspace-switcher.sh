79ade17aa4cb96fff06f227aef4ab2f21198b1d0
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-12-30 01:23:35 +0100

    Abuse the shortcuts bus of KDE to get multidimentional workspaces with moving

--- Plasma/dot-local/bin/tc-workspace-switcher.sh
+++ Plasma/dot-local/bin/tc-workspace-switcher.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+declare action="Switch"
+declare index=0
+
 desktops_count="$( qdbus-qt6 org.kde.KWin /VirtualDesktopManager count )"
 rows_count="$( qdbus-qt6 org.kde.KWin  /VirtualDesktopManager rows )"
 desktops_per_row="$(( desktops_count / rows_count ))"
@@ -8,11 +13,24 @@ current_desktop="$(( $( qdbus-qt6 org.kde.KWin /KWin currentDesktop ) - 1 ))"
 current_row="$(( current_desktop / desktops_per_row ))"
 currnet_column="$(( current_desktop % desktops_per_row ))"
 
-if [[ "${1}" -le 0 ]] || [[ "${1}" -gt "${desktops_per_row}" ]]; then
-    printf "[\e[31merror\e[0m]: The suplied index is out of range\n"
-    printf "desktop-%i:\n\tDESKTOPS: %i\n\tROWS:     %i\n\tDPR:      %i\n\tCDESKTOP: %i\n\tCROW:     %i\n\tCCOL:     %i\n" "$current_desktop" "$desktops_count" "$rows_count" "$desktops_per_row" "$(( current_desktop + 1 ))" "$(( current_row + 1 ))" "$(( currnet_column + 1 ))"
+if [[ -z ${1+x} ]]; then
+    printf "desktop-%i:\n\tDESKTOPS: %i\n\tROWS:     %i\n\tDPR:      %i\n\tCDESKTOP: %i\n\tCROW:     %i\n\tCCOL:     %i\n" "${current_desktop}" "${desktops_count}" "${rows_count}" "${desktops_per_row}" "$(( current_desktop + 1 ))" "$(( current_row + 1 ))" "$(( currnet_column + 1 ))"
+    exit 0
+fi
+
+if ! [[ -z ${2+x} ]] && [[ "${2}" == "move" ]]; then
+    action="Window"
+fi
+
+if [[ "${1}" == "up" ]]; then
+    index="$(( (current_row - 1) * desktops_per_row + (currnet_column + 1) ))"
+elif [[ "${1}" == "down" ]]; then
+    index="$(( (current_row + 1) * desktops_per_row + (currnet_column + 1) ))"
+elif [[ "${1}" -gt "0" ]] && [[ "${1}" -le "${desktops_per_row}" ]]; then
+    index="$(( current_row * desktops_per_row + ${1} ))"
+else
     exit 1
 fi
 
-qdbus-qt6 org.kde.KWin /KWin setCurrentDesktop "$(( current_row * desktops_per_row + ${1} ))"
+qdbus-qt6 org.kde.kglobalaccel /component/kwin invokeShortcut "${action} to Desktop ${index}"
 
