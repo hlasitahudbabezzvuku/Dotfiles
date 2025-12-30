8b3f94d8b53afcd6d281695b63ebc4d309ed393a
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-12-30 15:04:53 +0100

    Adapt workspace switcher so it works with the new workspace monitor script

--- Plasma/dot-local/bin/tc-workspace-switcher.sh
+++ Plasma/dot-local/bin/tc-workspace-switcher.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
-declare action="Switch"
-declare index=0
+readonly CACHE_DIRECTORY="/tmp/tc-workspace-monitor"
+
+action="Switch"
+index=0
 
 desktops_count="$( qdbus-qt6 org.kde.KWin /VirtualDesktopManager count )"
 rows_count="$( qdbus-qt6 org.kde.KWin  /VirtualDesktopManager rows )"
@@ -23,9 +25,17 @@ if ! [[ -z ${2+x} ]] && [[ "${2}" == "move" ]]; then
 fi
 
 if [[ "${1}" == "up" ]]; then
-    index="$(( (current_row - 1) * desktops_per_row + (currnet_column + 1) ))"
+    if [[ -d ${CACHE_DIRECTORY} ]] && [[ -f ${CACHE_DIRECTORY}/"$(( current_row - 1 ))" ]]; then
+        index="$(( (current_row - 1) * desktops_per_row + ( $( <${CACHE_DIRECTORY}/"$(( current_row - 1 ))")) + 1))"
+    else
+        index="$(( (current_row - 1) * desktops_per_row + (currnet_column + 1) ))"
+    fi
 elif [[ "${1}" == "down" ]]; then
-    index="$(( (current_row + 1) * desktops_per_row + (currnet_column + 1) ))"
+    if [[ -d ${CACHE_DIRECTORY} ]] && [[ -f ${CACHE_DIRECTORY}/"$(( current_row + 1 ))" ]]; then
+        index="$(( (current_row + 1) * desktops_per_row + ( $( <${CACHE_DIRECTORY}/"$(( current_row + 1 ))")) + 1))"
+    else
+        index="$(( (current_row + 1) * desktops_per_row + (currnet_column + 1) ))"
+    fi
 elif [[ "${1}" -gt "0" ]] && [[ "${1}" -le "${desktops_per_row}" ]]; then
     index="$(( current_row * desktops_per_row + ${1} ))"
 else
