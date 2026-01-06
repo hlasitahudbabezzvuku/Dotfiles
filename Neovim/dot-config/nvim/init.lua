180c266e7cf2a675ead8c4b1f26f541e06e5b961
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-01-07 00:09:35 +0100

    Correct to single qotes (again)

--- Neovim/dot-config/nvim/init.lua
+++ Neovim/dot-config/nvim/init.lua
@@ -9,12 +9,12 @@ require('config.keybinds')
 
 require('config.lazy')
 
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'VeryLazy',
   callback = function()
-      for _, file in ipairs(vim.api.nvim_get_runtime_file("lua/modules/*.lua", true)) do
-          local ok, module = pcall(require, "modules." .. vim.fn.fnamemodify(file, ":t:r"))
-          if ok and type(module) == "table" and module.setup then
+      for _, file in ipairs(vim.api.nvim_get_runtime_file('lua/modules/*.lua', true)) do
+          local ok, module = pcall(require, 'modules.' .. vim.fn.fnamemodify(file, ':t:r'))
+          if ok and type(module) == 'table' and module.setup then
               module.setup()
           end
       end
