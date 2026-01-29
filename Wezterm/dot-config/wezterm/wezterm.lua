22c975b28900e6060e2b5c9258ae37590ada03a7
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-01-29 08:35:06 +0100

    Make nerd fon gliphs in wezterm smaller

--- Wezterm/dot-config/wezterm/wezterm.lua
+++ Wezterm/dot-config/wezterm/wezterm.lua
@@ -1,6 +1,9 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font_with_fallback({
+    'JetBrains Mono',
+    { family = 'Symbols Nerd Font Mono', scale = 0.75 },
+})
 
 return config
