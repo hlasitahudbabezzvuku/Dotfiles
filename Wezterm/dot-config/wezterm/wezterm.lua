fc5d8741e600672281d30a7c3f2c9661a9e95ce3
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-01-29 08:53:26 +0100

    Add proper colors to wezterm

--- Wezterm/dot-config/wezterm/wezterm.lua
+++ Wezterm/dot-config/wezterm/wezterm.lua
@@ -6,4 +6,37 @@ config.font = wezterm.font_with_fallback({
     { family = 'Symbols Nerd Font Mono', scale = 0.75 },
 })
 
+config.colors = {
+    foreground = '#c9d1d9',
+    background = '#000000',
+
+    cursor_bg = '#c9d1d9',
+    cursor_fg = '#000000',
+    cursor_border = '#c9d1d9',
+
+    selection_fg = '#000000',
+    selection_bg = '#5d63f0',
+
+    ansi = {
+        '#161b22',
+        '#ff7b72',
+        '#7ee787',
+        '#ffa657',
+        '#58a6ff',
+        '#797ef9',
+        '#d2a8ff',
+        '#c9d1d9',
+    },
+    brights = {
+        '#21262d',
+        '#ffa198',
+        '#aff5b4',
+        '#ffc680',
+        '#79c0ff',
+        '#939aff',
+        '#e2c5ff',
+        '#f0f6fc',
+    },
+}
+
 return config
