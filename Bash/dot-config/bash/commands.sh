0e93b5bb0a5269efc2cbe9c145b468b63b484f8e
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-01-30 21:56:11 +0100

    Merge the vi.sh into the commands.sh

--- Bash/dot-config/bash/commands.sh
+++ Bash/dot-config/bash/commands.sh
@@ -1,3 +1,15 @@
+vi ()
+{
+    local dir="$( vifm --choose-dir - "$@" )"
+
+    if [ -z "$dir" ]; then
+        printf "Directory picking cancelled/failed\n"
+        return 1
+    fi
+
+    cd "$dir" || return 1
+}
+
 tt ()
 {
     local name="$( date -I ).${*}"
