54fb906700a97f41ec079a0ed43add1e00b12bb4
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-02-21 20:44:23 +0100

    Add telescope-tabs plugin for switching tabs

--- Neovim/dot-config/nvim/lua/plugins/telescope.lua
+++ Neovim/dot-config/nvim/lua/plugins/telescope.lua
@@ -1,29 +1,42 @@
 -- TODO: https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#git-pickers
 
 return {
-    'nvim-telescope/telescope.nvim',
-    dependecies = { 'nvim-tree/nvim-web-devicons' },
-    cmd = 'Telescope',
-    keys = {
-        { '<leader>f', function() require('telescope.builtin').find_files() end,  desc = 'Find files' },
-        { '<leader>g', function() require('telescope.builtin').live_grep() end,   desc = 'Live grep' },
-        { '<leader>b', function() require('telescope.builtin').buffers() end,     desc = 'Find buffers' },
-        { '<leader>k', function() require('telescope.builtin').diagnostics() end, desc = 'Find diagnostics' },
-        { '<leader>;', function() require('telescope.builtin').builtin() end,     desc = 'Telescope' },
-    },
-    opts = {
-        defaults = {
-            layout_strategy = 'horizontal',
-            layout_config = {
-                horizontal = {
-                    prompt_position = 'top',
-                    width = { padding = 0 },
-                    height = { padding = 0 },
-                    preview_width = 0.5,
-                },
-            },
-            sorting_strategy = 'ascending',
-            path_display = { 'filename_first' }
+    {
+        'nvim-telescope/telescope.nvim',
+        dependecies = { 'nvim-tree/nvim-web-devicons' },
+        cmd = 'Telescope',
+        keys = {
+            { '<leader>f', function() require('telescope.builtin').find_files() end,  desc = 'Find files' },
+            { '<leader>g', function() require('telescope.builtin').live_grep() end,   desc = 'Live grep' },
+            { '<leader>b', function() require('telescope.builtin').buffers() end,     desc = 'Find buffers' },
+            { '<leader>k', function() require('telescope.builtin').diagnostics() end, desc = 'Find diagnostics' },
+            { '<leader>;', function() require('telescope.builtin').builtin() end,     desc = 'Telescope' },
         },
+        opts = {
+            defaults = {
+                layout_strategy = 'horizontal',
+                layout_config = {
+                    horizontal = {
+                        prompt_position = 'top',
+                        width = { padding = 0 },
+                        height = { padding = 0 },
+                        preview_width = 0.5,
+                    },
+                },
+                sorting_strategy = 'ascending',
+                path_display = { 'filename_first' }
+            },
+        }
+    },
+    {
+        'LukasPietzschmann/telescope-tabs',
+        keys = {
+            { '<leader>t', function() require('telescope-tabs').list_tabs() end, desc = 'Find tabs' },
+        },
+        config = function()
+            require('telescope').load_extension 'telescope-tabs'
+            require('telescope-tabs').setup {}
+        end,
+        dependencies = { 'nvim-telescope/telescope.nvim' },
     }
 }
