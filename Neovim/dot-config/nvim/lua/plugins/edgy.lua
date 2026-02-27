559e4547ebfc5b81762d43cfc539b40433ceb26f
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-02-27 01:02:55 +0100

    Add the edgy.nvim plugin for managing the Neovim windows

---
+++ Neovim/dot-config/nvim/lua/plugins/edgy.lua
@@ -0,0 +1,58 @@
+return {
+    'folke/edgy.nvim',
+    event = 'VeryLazy',
+    init = function()
+        vim.api.nvim_create_autocmd('BufReadPost', {
+            callback = function(args)
+                local ft = vim.bo[args.buf].filetype
+                if not vim.tbl_contains({ 'gitcommit', 'gitrebase' }, ft) then
+                    require('edgy').open()
+                end
+            end,
+        })
+    end,
+    opts = {
+        right = {
+            {
+                title = 'Filesystem',
+                ft = 'neo-tree',
+                filter = function(buf)
+                    return vim.b[buf].neo_tree_source == 'filesystem'
+                end,
+                pinned = true,
+                open = 'Neotree show position=left filesystem',
+            },
+            {
+                title = 'Git',
+                ft = 'neo-tree',
+                filter = function(buf)
+                    return vim.b[buf].neo_tree_source == 'git_status'
+                end,
+                pinned = true,
+                open = 'Neotree show position=right git_status',
+                size = { height = 0.35 },
+            },
+        },
+        bottom = {
+            {
+                title = 'Diagnostics',
+                ft = 'trouble',
+            },
+            {
+                ft = 'qf',
+                title = 'QuickFix'
+            },
+            {
+                ft = 'help',
+            },
+        },
+        exit_when_last = true,
+        animate = { enabled = false },
+        options = {
+            left = { size = 35 },
+            bottom = { size = 10 },
+            right = { size = 35 },
+            top = { size = 10 },
+        },
+    },
+}
