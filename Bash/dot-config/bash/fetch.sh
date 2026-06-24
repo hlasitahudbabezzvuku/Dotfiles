e5c5c7c3568f174d784f2d9e0da772c303669843
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-06-24 21:52:20 +0200

    Remove all theming from Bash configuration (commented out)

--- Bash/dot-config/bash/fetch.sh
+++ Bash/dot-config/bash/fetch.sh
@@ -1,12 +1,11 @@
-printf '\n    _/        _/        _/               '
-printf '\n   _/_/_/    _/_/_/    _/_/_/    _/_/_/_/'
-printf '\n  _/    _/  _/    _/  _/    _/      _/   '
-printf '\n _/    _/  _/    _/  _/    _/    _/      '
-printf '\n_/    _/  _/    _/  _/_/_/    _/_/_/_/   \n'
-
-printf '\n%s┌─────────── %sHlasitaHudbaBezZvuku%s ───────────┐%s' "$( tput setaf 8 )" "$( tput setaf 4 )" "$( tput setaf 8 )" "$( tput sgr0 )"
-printf '\n  user:       %s' "$( whoami )"
-printf '\n  hostname:   %s' "$( hostname )"
-printf '\n  uptime:     %s' "$( uptime -p )"
-printf '\n%s└────────────────────────────────────────────┘%s\n' "$( tput setaf 8 )" "$( tput sgr0 )"
-
+# printf '\n    _/        _/        _/               '
+# printf '\n   _/_/_/    _/_/_/    _/_/_/    _/_/_/_/'
+# printf '\n  _/    _/  _/    _/  _/    _/      _/   '
+# printf '\n _/    _/  _/    _/  _/    _/    _/      '
+# printf '\n_/    _/  _/    _/  _/_/_/    _/_/_/_/   \n'
+#
+# printf '\n%s┌─────────── %sHlasitaHudbaBezZvuku%s ───────────┐%s' "$( tput setaf 8 )" "$( tput setaf 4 )" "$( tput setaf 8 )" "$( tput sgr0 )"
+# printf '\n  user:       %s' "$( whoami )"
+# printf '\n  hostname:   %s' "$( hostname )"
+# printf '\n  uptime:     %s' "$( uptime -p )"
+# printf '\n%s└────────────────────────────────────────────┘%s\n' "$( tput setaf 8 )" "$( tput sgr0 )"
