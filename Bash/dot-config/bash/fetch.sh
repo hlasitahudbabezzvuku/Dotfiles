d993d069b8bcf4a04a3d8f7c317375c5e8297950
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-07-14 18:24:34 +0200

    Create a new fetch script for bash

--- Bash/dot-config/bash/fetch.sh
+++ Bash/dot-config/bash/fetch.sh
@@ -1,41 +1,33 @@
-c1="38;2;255;255;255"
-c2="0;34"
+(
+    readonly -a FETCH_CHARS=(               # List of names that are used for selecting the names of the Belora sessions
+        "🬐" "🬑" "🬒" "🬓" "🬔"
+        "🬕" "🬖" "🬗" "🬘" "🬙"
+        "🬚" "🬛" "🬜" "🬠" "🬡"
+        "🬢" "🬣" "🬤" "🬥" "🬦"
+        "🬧" "🬨" "🬩" "🬪" "🬫"
+        " " " " " " " " " "
+    )
 
-readonly -a FETCH_CHARS=(               # List of names that are used for selecting the names of the Belora sessions
-    "🬐" "🬑" "🬒" "🬓" "🬔"
-    "🬕" "🬖" "🬗" "🬘" "🬙"
-    "🬚" "🬛" "🬜" "🬠" "🬡"
-    "🬢" "🬣" "🬤" "🬥" "🬦"
-    "🬧" "🬨" "🬩" "🬪" "🬫"
-    " " " " " " " " " "
+    noise ()
+    {
+        for ((i = 0; i < 20; i++)); do
+            printf "%s" "${FETCH_CHARS[ $RANDOM % ${#FETCH_CHARS[@]} ]}"
+            done
+    }
+
+    printf "\n%s   ╭─────────── HlasitaHudbaBezZvuku ───────────╮" "$( noise )"
+    printf "\n%s     user:        %s"                              "$( noise )" "${USER}"
+    printf "\n%s     hostname:    %s"                              "$( noise )" "${HOSTNAME}"
+    printf "\n%s   ╰────────────────────────────────────────────╯" "$( noise )"
+
+    if [ -f /etc/os-release ]; then
+        source /etc/os-release
+        printf "\n%s   ╭────────────────────────────────────────────╮" "$( noise )"
+        printf "\n%s     name:        %s"                              "$( noise )" "${NAME}"
+        printf "\n%s     variant:     %s"                              "$( noise )" "${VARIANT}"
+        printf "\n%s     version:     %s"                              "$( noise )" "${VERSION_ID}"
+        printf "\n%s   ╰────────────────────────────────────────────╯" "$( noise )"
+    fi
+
+    printf "\n"
 )
-
-noise ()
-{
-    for ((i = 0; i < ${1}; i++)); do
-        printf "%s" "${FETCH_CHARS[ $RANDOM % ${#FETCH_CHARS[@]} ]}"
-    done
-}
-
-printf "       ╭──────╮\n"
-printf "       │%s│\n" "$( noise 6 )"
-printf "       │%s│\n" "$( noise 6 )"
-printf "╭──────┼──────╯\n"
-printf "│%s│\n" "$( noise 6 )"
-printf "│%s│\n" "$( noise 6 )"
-printf "╰──────╯\n"
-
-# printf "\033[%sm      _____\n"                            "${c2}"
-# printf "     /   __)\033[%sm%s\033[%sm\n"                 "${c1}" "\\" "${c2}"
-# printf "     |  /  \033[%sm\\ %s\033[%sm\n"               "${c1}" "\\" "${c2}"
-# printf "  \033[%sm__\033[%sm_|  |_\033[%sm_/ /\033[%sm\n" "${c1}" "${c2}" "${c1}" "${c2}"
-# printf " \033[%sm/ \033[%sm(_    _)\033[%sm_/\033[%sm\n"  "${c1}" "${c2}" "${c1}" "${c2}"
-# printf "\033[%sm/ /\033[%sm  |  |\n"                      "${c1}" "${c2}"
-# printf "\033[%sm\\ %s\033[%sm__/  |\n"                    "${c1}" "\\" "${c2}"
-# printf " \033[%sm%s\033[%sm(_____/\n"                     "${c1}" "\\" "${c2}"
-
-# printf '\n%s┌─────────── %sHlasitaHudbaBezZvuku%s ───────────┐%s' "$( tput setaf 8 )" "$( tput setaf 4 )" "$( tput setaf 8 )" "$( tput sgr0 )"
-# printf '\n  user:       %s' "$( whoami )"
-# printf '\n  hostname:   %s' "$( hostname )"
-# printf '\n  uptime:     %s' "$( uptime -p )"
-# printf '\n%s└────────────────────────────────────────────┘%s\n' "$( tput setaf 8 )" "$( tput sgr0 )"
