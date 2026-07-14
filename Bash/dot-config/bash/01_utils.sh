018671fbe4e79cd685ffd9954c304a89229d75c2
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-07-14 18:38:05 +0200

    Add separate utils file for bash

---
+++ Bash/dot-config/bash/01_utils.sh
@@ -0,0 +1,15 @@
+rgb ()
+{
+    if [[ "${1}" == "reset" ]]; then
+        printf "\033[0m"
+    elif [[ "${1}" == "fg" ]]; then
+        printf "\033[38;2;%i;%i;%im" "${2}" "${3}" "${4}"
+    elif [[ "${1}" == "bg" ]]; then
+        printf "\033[48;2;%i;%i;%im" "${2}" "${3}" "${4}"
+    elif [[ "${1}" == "ansi_fg" ]]; then
+        printf "\033[0;3%im" "${2}"
+    elif [[ "${1}" == "ansi_bg" ]]; then
+        printf "\033[4%im" "${2}"
+    fi
+}
+
