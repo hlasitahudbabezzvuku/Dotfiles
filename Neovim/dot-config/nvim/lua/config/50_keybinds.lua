219753cfa7dda0a3f09a021941d1812e941eb08a
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-08-09 19:10:57 +0200

    Remove the code disabling arrows

--- Neovim/dot-config/nvim/lua/config/50_keybinds.lua
+++ Neovim/dot-config/nvim/lua/config/50_keybinds.lua
@@ -10,12 +10,6 @@ vim.keymap.set('n', '<leader>cu', function() vim.cmd('cd ' .. vim.fn.expand('..'
 -- QoL
 vim.keymap.set('n', '<esc>', '<cmd>nohlsearch<cr>')
 
--- Disable arrows
-vim.keymap.set({ 'n', 'i', 'x', 'v' }, '<up>', '<nop>', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'i', 'x', 'v' }, '<down>', '<nop>', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'i', 'x', 'v' }, '<left>', '<nop>', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'i', 'x', 'v' }, '<right>', '<nop>', { noremap = true, silent = true })
-
 -- Enable navigation using c-a-<direction>
 vim.keymap.set({ 'n', 'i', 'x', 'v' }, '<c-a-k>', '<up>', { noremap = true, silent = true })
 vim.keymap.set({ 'n', 'i', 'x', 'v' }, '<c-a-j>', '<down>', { noremap = true, silent = true })
