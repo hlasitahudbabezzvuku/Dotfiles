9deb0dcfa518fb79a255bcfbaec1feb9343b9043
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-08-09 17:58:48 +0200

    Add automatic sourcing of configuration

--- Neovim/dot-config/nvim/init.lua
+++ Neovim/dot-config/nvim/init.lua
@@ -4,6 +4,7 @@
 -- /_/|_/\__/\___/___/_/_/_/_/
 
 
-require('config.options')
-require('config.keybinds')
-require('config.lazy')
+for _, file in ipairs(vim.api.nvim_get_runtime_file('lua/config/*.lua', true)) do
+    local mod_name = vim.fn.fnamemodify(file, ':t:r')
+    require('config.' .. mod_name)
+end
