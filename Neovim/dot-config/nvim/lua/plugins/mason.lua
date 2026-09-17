27314b1554b990e28b21698bad322df82c864034
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-09-17 10:11:00 +0200

    Add mason-lspconfig for automatic lsp registration

--- Neovim/dot-config/nvim/lua/plugins/mason.lua
+++ Neovim/dot-config/nvim/lua/plugins/mason.lua
@@ -1,16 +1,30 @@
 return {
-    'mason-org/mason.nvim',
-    cmd = 'Mason',
-    event = 'VeryLazy',
-    opts = {
-        ui = {
-            border = 'rounded',
-            width = 0.8,
-            height = 0.7,
-            icons = {
-                package_installed = '',
-                package_pending = '',
-                package_uninstalled = ''
+    {
+        'mason-org/mason-lspconfig.nvim',
+        event = 'BufRead',
+        opts = {
+            automatic_enable = true,
+            ensure_installed = { "lua_ls" },
+        },
+        dependencies = {
+            'mason-org/mason.nvim',
+            'neovim/nvim-lspconfig',
+        },
+    },
+    {
+        'mason-org/mason.nvim',
+        cmd = 'Mason',
+        event = 'VeryLazy',
+        opts = {
+            ui = {
+                border = 'rounded',
+                width = 0.8,
+                height = 0.7,
+                icons = {
+                    package_installed = '',
+                    package_pending = '',
+                    package_uninstalled = '',
+                }
             }
         }
     }
