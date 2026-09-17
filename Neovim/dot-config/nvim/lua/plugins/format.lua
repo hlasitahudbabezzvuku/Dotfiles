4eff6ae8674288e0aff94fad94e367a179d6e4fa
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-09-17 11:40:30 +0200

    Add format on save to neovim

--- Neovim/dot-config/nvim/lua/plugins/format.lua
+++ Neovim/dot-config/nvim/lua/plugins/format.lua
@@ -1,7 +1,13 @@
 return {
     'stevearc/conform.nvim',
     event = 'BufRead',
+    keys = {
+        { 'grf', function() require('conform').format() end, desc = 'conform.format()' },
+    },
     opts = {
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
         format_on_save = {
             timeout_ms = 500,
             lsp_format = 'fallback',
