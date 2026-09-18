3541e4ac115d7de11764388ea90412d1a14a7080
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-09-18 20:36:17 +0200

    Replace double-quotes with single-quotes in format.lua

--- Neovim/dot-config/nvim/lua/plugins/format.lua
+++ Neovim/dot-config/nvim/lua/plugins/format.lua
@@ -6,7 +6,7 @@ return {
     },
     opts = {
         default_format_opts = {
-            lsp_format = "fallback",
+            lsp_format = 'fallback',
         },
         format_on_save = {
             timeout_ms = 500,
