0a4f706bb6e3247375d9bed8178486ee72ec8c6c
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-09-18 20:04:38 +0200

    Add shortcut for enabling inlay hints

--- Neovim/dot-config/nvim/lua/plugins/lsp.lua
+++ Neovim/dot-config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,9 @@
 return {
     'neovim/nvim-lspconfig',
-    config = function ()
-        vim.lsp.enable('clangd')
+    keys = {
+        { 'grl', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, desc = 'conform.format()' },
+    },
+    config = function()
+        vim.lsp.enable({ 'clangd', 'lua_ls', 'shellcheck' })
     end
 }
